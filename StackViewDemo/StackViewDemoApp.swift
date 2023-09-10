9cbd2882ac367c3b416da97098bfcfd6daf6f84b
Sorfian <sorfian.tkj@gmail.com>
2023-09-10 11:12:06 +0700

    Initial Commit

---
+++ StackViewDemo/StackViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackViewDemoApp.swift
+//  StackViewDemo
+//
+//  Created by Sorfian on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct StackViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
